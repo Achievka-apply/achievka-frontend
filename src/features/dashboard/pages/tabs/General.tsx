7c174be71f46cbea7311e07fe7e9dbdb980fa536
tincenma <temirlanilyasov1@gmail.com>
2025-05-01 00:48:47 +0500

    Add text to contents pages

--- src/features/dashboard/pages/tabs/General.tsx
+++ src/features/dashboard/pages/tabs/General.tsx
@@ -1,7 +1,14 @@
 export default function General() {
     return (
         <>
-            <h1>General</h1>
+            <h1>Hi, [Name]! Ready to take the next step toward your future?</h1>
+
+            Здесь отображаются все ваши последние действия и шаги. Удобно вернуться к важному — всё под рукой.
+        
+            Все инструменты по гриду
+
+            График сколько осталось запросов на каждый инструмент 
+
         </>
     );
 };
