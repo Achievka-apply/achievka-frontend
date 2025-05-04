88c7460b95fcda419bd50175bfe2c19a0ed9aef7
tincenma <temirlanilyasov1@gmail.com>
2025-05-04 22:04:18 +0500

    Add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,20 @@
+export default function Logo() {
+    return (
+        <>
+            <button
+                className="mb-4"
+                style={{
+                    fontSize: '1.5rem',
+                    width: '48px',
+                    height: '48px',
+                    borderRadius: '50%',
+                    backgroundColor: '#fff',
+                    color: '#111',
+                    border: 'none',
+                }}
+            >
+                A
+            </button>
+        </>
+    )
+}
