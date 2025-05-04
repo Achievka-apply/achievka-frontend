1f524784a496980ffab3bfaa2ff4a7befd275753
tincenma <temirlanilyasov1@gmail.com>
2025-05-04 22:24:29 +0500

    Set up Redux - Create userSlice for user data - Create interface for user data - Configure global store - Create reducers for user data: setUserProfile and clearUserProfile - Create useAppSelector and useAppDispatch hooks

---
+++ src/store/user/userTypes.ts
@@ -0,0 +1,14 @@
+// src/store/user/userTypes.ts
+
+export interface UserProfile {
+    userId: string;
+    email: string;
+    name: string;
+    surname: string;
+    country: string;
+    subscription: 'free' | 'plan1' | 'plan2' | 'plan3';
+    phoneNumber: string;
+    dateOfBirth: string;
+    language: 'en' | 'kk' | 'ru';
+  }
+  
